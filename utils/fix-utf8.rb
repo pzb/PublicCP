4548095653e22d0aa2d8a62d796be41e75df8ac8
Peter Bowen <pzb@amazon.com>
2015-04-05 21:35:31 -0700

    Update formatting to work with Word and PDF

---
+++ utils/fix-utf8.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+
+contents = nil
+File.open(ARGV[0], 'r:UTF-8') do |f|
+    contents = f.read
+end
+contents.strip!
+
+contents.each_char do |c|
+    case c.ord
+    # New line
+    when 0xa
+        puts
+    # Tab is 4 spaces
+    when 0x9
+        print "    "
+    # Printable ascii + space
+    when 0x20..0x7e
+        print c
+    # 'EN DASH' (U+2013)
+    when 0x2013
+        print "--"
+    # 'LEFT SINGLE QUOTATION MARK' (U+2018)
+    when 0x2018
+        print "'"
+    # 'RIGHT SINGLE QUOTATION MARK' (U+2019)
+    when 0x2019
+        print "'"
+    # 'LEFT DOUBLE QUOTATION MARK' (U+201C)
+    when 0x201c
+        print '"'
+    # 'RIGHT DOUBLE QUOTATION MARK' (U+201D)
+    when 0x201d
+        print '"'
+    else
+        $stderr.puts "\n0x#{c.ord.to_s(16)} is not allowed in #{ARGV[0]}!"
+        exit 10
+    end
+end
+puts
+
+# '
